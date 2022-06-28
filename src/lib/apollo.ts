1f3d57a24b2bf60f8747da947c11f76ef3ea0279
Phelype Rodrigo <81370546+phelype-dev@users.noreply.github.com>
2022-06-28 14:53:04 -0300

    cache

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -6,5 +6,5 @@ export const client = new ApolloClient({
   headers: {
     'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN} `,
   },
-  cache: new InMemoryCache(), // Cache das requisições
+  cache: new InMemoryCache(dataIdFromObject), // Cache das requisições
 });
